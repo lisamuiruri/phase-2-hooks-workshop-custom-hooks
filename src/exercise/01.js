6fd337bb94f06acbda381078d4137f454f900337
lisa <lisawanjiku41@gmail.com>
2025-07-29 13:48:36 +0300

    phase-2-hooks-workshop-custom-hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,24 +1,24 @@
 import { useEffect } from "react";
 
-/* ✅ create a new function called useDocumentTitle */
-/* 👀 export function useDocumentTitle() {} */
+// ✅ Create the custom hook
+export function useDocumentTitle(title = "Welcome to the home page!") {
+  useEffect(() => {
+    document.title = title;
+  }, [title]); // 👀 Don't forget to add title to the dependencies array
+}
 
 export default function Home() {
-  /* 
-    ✅ move the useEffect code into your useDocumentTitle function
-   then, call the useDocumentTitle hook in your component
-  */
-  useEffect(() => {
-    document.title = "Welcome to the home page!";
-  }, []);
+  // ✅ Call the custom hook in your component
+  useDocumentTitle("Welcome to the home page!");
 
   return (
     <div>
       <h1>Home Page</h1>
       <p>
         To see the title change in the browser tab, click the 'Open in new tab'
-        link above
+        link above.
       </p>
     </div>
   );
 }
+
